8ed1012a4c551a6fd1654fdcd99995b46106f3e0
yousrasrk <39521130+yousrasrk@users.noreply.github.com>
2020-12-18 00:33:51 +0100

    Add files via upload

---
+++ ProductMocrosoftService/ProductMocrosoftService/Model/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductMocrosoftService.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+
+    }
+}
